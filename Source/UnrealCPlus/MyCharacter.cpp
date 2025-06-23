7a9fbfcd7b7a15b295da877637917af87a7c8659
lecture <ksw617@gmail.com>
2025-06-23 21:43:15 +0900

    update

--- Source/UnrealCPlus/MyCharacter.cpp
+++ Source/UnrealCPlus/MyCharacter.cpp
@@ -12,7 +12,7 @@ AMyCharacter::AMyCharacter()
  	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
-	static ConstructorHelpers::FObjectFinder<USkeletalMesh> SM(TEXT("/Script/Engine.SkeletalMesh'/Game/ParagonGreystone/Characters/Heroes/Greystone/Meshes/Greystone.Greystone'"));
+	static ConstructorHelpers::FObjectFinder<USkeletalMesh> SM(TEXT("/Script/Engine.SkeletalMesh'/Game/ParagonSparrow/Characters/Heroes/Sparrow/Meshes/Sparrow.Sparrow'"));
 
 	if (SM.Succeeded())
 	{
